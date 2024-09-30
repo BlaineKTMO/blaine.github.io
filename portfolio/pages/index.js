11596e06e046b42d77312173e436a9391fdede1c
Blaine Oania <blaine.oania@gmail.com>
2024-09-29 23:40:35 -0400

    Add Google site verification meta tag

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -64,6 +64,7 @@ export default function Home() {
             <Head>
                 <title>About Me</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="google-site-verification" content="04j-0yUDW2oBFuoMn83sJ_dPGHnmBymiGppHJheaw0o" />
             </Head>
             <ReactFullpage
                 navigation
